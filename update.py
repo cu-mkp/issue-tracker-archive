6b1085695fa716a9e46e49fa05cd7ff4c2248b28
gschare <grschare1315@gmail.com>
2020-12-03 00:02:53 -0800

    Update comments.json and issues.json.

--- update.py
+++ update.py
@@ -2,29 +2,36 @@ import json
 import requests
 from config import token # Get the secret token from a file called config.py.
 
-PER_PAGE = 3
-PAGES = 1
+PER_PAGE = 100
+PAGES = 30
 REQUEST_URL = "https://api.github.com/repos/cu-mkp/m-k-manuscript-data/issues"
 
 def get_issues(request_url=REQUEST_URL):
     issues = []
     for page in range(1, PAGES+1): # Is there a better way to make sure we get all the issues than by picking a large number of pages?
-        payload = {"state":"all", "page":page, "per_page":PER_PAGE, "sort":"created", "direction":"desc"}
+        payload = {"state":"all", "page":page, "per_page":PER_PAGE, "sort":"created", "direction":"asc"}
         r = requests.get(request_url, params=payload, headers={"Authorization":f"token {token}"})
-        print(r.status_code)
+        assert r.status_code == 200
+        print(f"Found {len(r.json())} issues on page {page}.")
         issues.extend(r.json())
+        if len(r.json()) < PER_PAGE:
+            break
+    print(f"Found {len(issues)} issues.")
     return issues
 
 def get_comments(issues):
     comments = {}
     for issue in issues:
+        if issue["comments"] == 0:
+            continue
         comments[issue["id"]] = []
-        print(issue["comments"]/PER_PAGE)
         for page in range(1, issue["comments"]//PER_PAGE+2):
-            print("looping loop")
             payload = {"state":"all", "page":page, "per_page":PER_PAGE, "sort":"created", "direction":"asc"}
             r = requests.get(issue["comments_url"], params=payload, headers={"Authorization":f"token {token}"})
+            assert r.status_code == 200
             comments[issue["id"]].extend(r.json())
+        assert len(comments[issue["id"]]) == issue["comments"]
+        print(f"Found {len(comments[issue['id']])}/{issue['comments']} comments for issue #{issue['number']}.")
     return comments
 
 def write_issues(issues):
